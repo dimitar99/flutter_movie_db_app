5589c1eda738c6c99cb5a80b7b06a24227e7439d
Dimitar Emilov Tochev <dimitar1999.de@gmail.com>
2025-03-08 20:32:33 +0100

    Finished initial configuration

---
+++ lib/core/config/network/dio_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_movie_db_app/core/config/env/env_constants.dart';
+import 'package:flutter_movie_db_app/core/config/network/interceptors/auth_interceptor.dart';
+import 'package:flutter_movie_db_app/core/config/network/interceptors/logger_interceptor.dart';
+
+class DioService {
+  static final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.baseUrl,
+      connectTimeout: const Duration(seconds: 20),
+      receiveTimeout: const Duration(seconds: 20),
+      sendTimeout: const Duration(seconds: 15),
+      contentType: 'application/json',
+    ),
+  )..interceptors.addAll([AuthInterceptor(), LoggerInterceptor()]);
+}
